8a126d69aace90078e22b5025683ff0f164cc8e2
S <stacey.blaschke@gmail.com>
2024-12-31 14:02:34 -0800

    Added tailwind CSS to project and converting CSS to tailwind, files changed so far

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,ts,jsx,tsx,html}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        title: ['haymakerregular', 'cursive'],
+        mono: ['Fira Code', 'monospace'],
+        sans: [
+          'Open Sans',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial',
+          'Noto Sans',
+          'sans-serif',
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
