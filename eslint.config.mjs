ebe86569e4b8b4c95552358dd575f7ec39e89646
S <stacey.blaschke@gmail.com>
2025-01-01 01:22:09 -0800

    Update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,43 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import prettier from 'eslint-config-prettier';
-import reactPlugin from 'eslint-plugin-react';
+import js from "@eslint/js";
+import globals from "globals";
+import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 
 /** @type {import('eslint').Linter.Config[]} */
+/* .all is another option in place of .recoommended but can be overkill */,
 export default [
-  js.configs.recommended,
+  js.configs.recommended, 
   {
     ...reactPlugin.configs.flat.recommended,
     settings: {
       react: {
-        version: 'detect',
+        version: "detect",
       },
     },
   },
-  reactPlugin.configs.flat['jsx-runtime'],
+  reactPlugin.configs.flat["jsx-runtime"],
+  ...pluginQuery.configs["flat/recommended"],
   {
-    files: ['**/*.js', '**/*.jsx'],
+    files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
-      globals: { ...globals.browser },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
       parserOptions: {
         ecmaFeatures: {
-          impliedStrict: true,
-          jsx: true,
+          jsx: true /* add in jsx */,
         },
       },
     },
     rules: {
-      'react/no-unescaped-entities': 'off',
-      'react/prop-types': 'off',
+      "react/no-unescaped-entities": "off",
+      "react/prop-types": "off",
     },
   },
+  /* prettier always has to be last as it only turn off formatting, it does
+    not add anything, prevents eslint from warning about formatting things that 
+    do not matter as prettier will take care of it */
   prettier,
 ];
