d0caea9a636e6080bc533015da973f7a086f90bf
S <stacey.blaschke@gmail.com>
2025-01-04 23:35:21 -0800

    Added classnames prop tp the LinkButton, centered button text and added border-radius

--- src/components/ButtonLink.jsx
+++ src/components/ButtonLink.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-export default function LinkButton({ label, url, fontsize }) {
-  const cnames = `link text-${fontsize}`;
+export default function LinkButton({ label, url, fontsize, classNames = '' }) {
+  let cnames = `link text-${fontsize}`;
+  if (classNames) {
+    cnames = `${cnames} ${classNames}`;
+  }
   return (
     <p className={cnames}>
       <a
         href={url}
         target="_blank"
-        className="bg-[#13446e] p-2 text-white shadow-md inline-block mt-8 
+        className="bg-[#13446e] p-2 text-white shadow-md inline-block text-center rounded-sm mt-8 
         hover:bg-[#13446e80]"
         rel="noreferrer">
         {label}
