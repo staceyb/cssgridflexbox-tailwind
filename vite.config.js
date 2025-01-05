4fd9d9bffbe5be762661a35a26cc7db61b5ad9cc
S <stacey.blaschke@gmail.com>
2025-01-04 23:33:34 -0800

    Edited the vite.config fixed curly brace on type def and quptes on clearscreen

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,31 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import browserslistToEsbuild from 'browserslist-to-esbuild';
+//import { imagetools } from 'vite-imagetools'
 
 /**
- *  @type {import('vite').UserConfig)
+ *  @type {import('vite').UserConfig}
  */
 // https://vitejs.dev/config/
-export default defineConfig(() => {
-  return {
-    clearScreen: 'false',
-    base: '/',
-    build: {
-      target: browserslistToEsbuild(),
-    },
-    preview: {
-      port: 8080,
-    },
-    css: {
-      devSourcemap: true,
-    },
-    server: {
-      open: false,
-      port: 3000,
-    },
-    coverage: {
-      reporter: ['text', 'json', 'html'],
-    },
-    plugins: [react()],
-  };
+export default defineConfig({
+  clearScreen: false,
+  base: '/',
+  build: {
+    target: browserslistToEsbuild(),
+    assetsInlineLimit: 6144,
+  },
+  preview: {
+    port: 8080,
+  },
+  css: {
+    devSourcemap: true,
+  },
+  server: {
+    open: false,
+    port: 3000,
+  },
+  coverage: {
+    reporter: ['text', 'json', 'html'],
+  },
+  plugins: [react()],
 });
