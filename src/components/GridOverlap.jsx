edd40fbf6ef24955cf6ea4d5d43f4351857385cb
greenlotus <stacey.blaschke@gmail.com>
2025-01-18 15:30:02 -0800

    Changed GridOverlap to use vite-imagetools to create webp versions of the photos for performance reasons.

--- src/components/GridOverlap.jsx
+++ src/components/GridOverlap.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 
+import jupiterFallback from '/src/imgs/fem-jupiter.jpg?w=630';
+import jupiterWebpSrc from '/src/imgs/fem-jupiter.jpg?w=630;960&format=webp&as=srcset';
+
+import neptuneFallback from '/src/imgs/fem-neptune.jpg?w=630';
+import neptuneWebpSrc from '/src/imgs/fem-neptune.jpg?w=630;960&format=webp&as=srcset';
+
+import saturnFallback from '/src/imgs/fem-saturn.jpg?w=630';
+import saturnWebpSrc from '/src/imgs/fem-saturn.jpg?w=630;960&format=webp&as=srcset';
+
+import plutoFallback from '/src/imgs/fem-pluto.jpg?w=630';
+import plutoWebpSrc from '/src/imgs/fem-pluto.jpg?w=630;960&format=webp&as=srcset';
+
 function GridOverlap() {
+  console.log(jupiterWebpSrc);
   return (
     <div className="grid max-w-[1200px] mx-auto sm:grid-cols-[1fr_1fr] sm:gap-[1vw]">
       <figure className="grid grid-cols-[1fr] grid-rows-[60%_40%] items-center gap-8">
         <img
           className="row-start-1 row-end-3 col-start-1 col-end-2"
-          src="/images/fem-jupiter.jpg"
+          sizes="(max-width: 640px) calc(100vw-39px), 519px"
+          srcSet={jupiterWebpSrc}
+          src={jupiterFallback}
+          width="488"
+          height="488"
           alt="Jupiter and its Galilean moons."
         />
         <figcaption className="col-start-1 col-end-2 row-start-2 opacity-[0.6] row-end-3 bg-color-black text-color-white p4">
@@ -17,7 +34,11 @@ function GridOverlap() {
       <figure className="grid grid-cols-[1fr] grid-rows-[60%_40%] items-center gap-8">
         <img
           className="row-start-1 row-end-3 col-start-1 col-end-2"
-          src="/images/fem-saturn.jpg"
+          sizes="(max-width: 640px) calc(100vw-39px), 519px"
+          srcSet={saturnWebpSrc}
+          src={saturnFallback}
+          width="488"
+          height="488"
           alt="Saturn and its moons."
         />
         <figcaption className="col-start-1 col-end-2 row-start-2 opacity-[0.6] row-end-3 bg-color-black text-color-white p4">
@@ -28,7 +49,11 @@ function GridOverlap() {
       <figure className="grid grid-cols-[1fr] grid-rows-[60%_40%] items-center gap-8">
         <img
           className="row-start-1 row-end-3 col-start-1 col-end-2"
-          src="/images/fem-neptune.jpg"
+          sizes="(max-width: 640px) calc(100vw-39px), 519px"
+          srcSet={neptuneWebpSrc}
+          src={neptuneFallback}
+          width="488"
+          height="488"
           alt="Triton, Neptune's main moon, in front of Neptune."
         />
         <figcaption className="col-start-1 col-end-2 row-start-2 opacity-[0.6] row-end-3 bg-color-black text-color-white p4">
@@ -39,7 +64,11 @@ function GridOverlap() {
       <figure className="grid grid-cols-[1fr] grid-rows-[60%_40%] items-center gap-8">
         <img
           className="row-start-1 row-end-3 col-start-1 col-end-2"
-          src="/images/fem-pluto.jpg"
+          sizes="(max-width: 640px) calc(100vw-39px), 519px"
+          srcSet={plutoWebpSrc}
+          src={plutoFallback}
+          width="488"
+          height="488"
           alt="Pluto and Charon."
         />
         <figcaption className="col-start-1 col-end-2 row-start-2 opacity-[0.6] row-end-3 bg-color-black text-color-white p4">
