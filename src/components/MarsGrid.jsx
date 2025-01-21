2be3c6490e55303be71bfcdd23b41a1922ff6d5f
greenlotus <stacey.blaschke@gmail.com>
2025-01-20 16:58:39 -0800

    Changed Mars images to picture element and avifs instead of jjpgs

--- src/components/MarsGrid.jsx
+++ src/components/MarsGrid.jsx
@@ -1,37 +1,56 @@
 import React from 'react';
 /* Grid Info: Uses grid template areas */
 
+import PIA01546AVIFSrc from '/src/imgs/PIA01546~orig.jpg?w=980;601&format=avif&as=srcset';
+import PIA01546Fallback from '/src/imgs/PIA01546~orig.jpg?w=980';
+
+import PIA13593AVIFSrc from '/src/imgs/PIA13593~orig.jpg?w=980;601&format=avif&as=srcset';
+import PIA13593Fallback from '/src/imgs/PIA13593~orig.jpg?w=980';
+
+import PIA00407AVIFSrc from '/src/imgs/PIA00407~small.jpg?w=482;591&format=avif&as=srcset';
+import PIA00407Fallback from '/src/imgs/PIA00407~small.jpg?w=591';
+
+import PIA08528AVIFSrc from '/src/imgs/PIA08528~orig.jpg?w=482;591&format=avif&as=srcset';
+import PIA08528Fallback from '/src/imgs/PIA08528~orig.jpg?w=591';
+
+import PIA05208AVIFSrc from '/src/imgs/PIA05208~small.jpg?w=482;591&format=avif&as=srcset';
+import PIA05208Fallback from '/src/imgs/PIA05208~small.jpg?w=591';
+
 function MarsGrid() {
   return (
     <>
       <h1>Mars</h1>
       <div className="grid grid-cols-[1fr] [grid-template-areas:'sol10''mars''meteorite''microscope''intrepid'] sm:[grid-template-areas:'sol10_sol10''mars_meteorite''mars_microscope''intrepid_intrepid'] sm:grid-cols-[1fr_1fr] gap-[1rem] max-w-[980px] mx-auto">
-        <img
-          className="[grid-area:sol10]"
-          src="/images/PIA01546~orig.jpg"
-          alt="True Color of Mars - Pathfinder Sol 10 at Noon."
-        />
-        <img
-          className="[grid-area:mars]"
-          src="/images/PIA00407~small.jpg"
-          alt="Global color views of Mars"
-        />
-        <img
-          className="[grid-area:intrepid]"
-          src="/images/PIA13593~orig.jpg"
-          alt="Intrepid Crater on Mars"
-        />
-        <img
-          className="[grid-area:meteorite]"
-          src="/images/PIA08528~orig.jpg"
-          alt="Possible Meteorite in Columbia Hills on Mars False Color"
-        />
-
-        <img
-          className="[grid-area:microscope]"
-          src="/images/PIA05208~small.jpg"
-          alt="Mars Under the Microscope color"
-        />
+        <picture className="[grid-area:sol10]">
+          <source srcSet={PIA01546AVIFSrc} type="image/avif" />
+          <img
+            alt="True Color of Mars - Pathfinder Sol 10 at Noon"
+            src={PIA01546Fallback}
+          />
+        </picture>
+        <picture className="[grid-area:mars]">
+          <source srcSet={PIA00407AVIFSrc} type="image/avif" />
+          <img
+            alt="Global color views of Mars"
+            fetchPriority="high"
+            src={PIA00407Fallback}
+          />
+        </picture>
+        <picture className="[grid-area:intrepid]">
+          <source srcSet={PIA13593AVIFSrc} type="image/avif" />
+          <img alt="Intrepid Crater on Mars" src={PIA13593Fallback} />
+        </picture>
+        <picture className="[grid-area:meteorite]">
+          <source srcSet={PIA08528AVIFSrc} type="image/avif" />
+          <img
+            alt="Possible Meteorite in Columbia Hills on Mars False Color"
+            src={PIA08528Fallback}
+          />
+        </picture>
+        <picture className="[grid-area:microscope]">
+          <source srcSet={PIA05208AVIFSrc} type="image/avif" />
+          <img alt="Mars Under the Microscope color" src={PIA05208Fallback} />
+        </picture>
       </div>
     </>
   );
